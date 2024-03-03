f42b76c93d6a5b15df1076b2ba036dec7929cd94
Vignesh <svzvicky@gmail.com>
2024-03-04 00:10:09 +0530

    Initial Commit

---
+++ src/main/java/com/den/bacheuserservice/BacheUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.den.bacheuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacheUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacheUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/den/bacheuserservice/BacheUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.den.bacheuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacheUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
